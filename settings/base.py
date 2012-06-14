8449d7809b0d57801a764cdf3e3f33906283323d
crccheck <c@crccheck.com>
2012-06-14 16:27:44 -0500

    add django humanize to installed apps

--- settings/base.py
+++ settings/base.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'django.contrib.humanize',
 
     'armstrong.core.arm_layout',
     'thedp',
