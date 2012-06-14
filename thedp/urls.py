d5666d1f4aa8271ecbd2cf1903acee836375069d
crccheck <c@crccheck.com>
2012-06-14 16:07:22 -0500

    convert instution detail view to generic detailview

--- thedp/urls.py
+++ thedp/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, url
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from .models import Institution, System
@@ -8,8 +9,8 @@ from .views import RenderModelDetailView
 urlpatterns = patterns('',
     url(r'^institution/$', ListView.as_view(
         queryset=Institution.objects.all().order_by('name')), name="institution_list"),
-    url(r'^institution/(?P<slug>[-\w]+)/$', RenderModelDetailView.as_view(
-        model=Institution, layout="table"), name="institution_detail"),
+    url(r'^institution/(?P<slug>[-\w]+)/$', DetailView.as_view(
+        model=Institution), name="institution_detail"),
     url(r'^system/$', ListView.as_view(
         model=System), name="system_list"),
     url(r'^system/(?P<slug>[-\w]+)/$', RenderModelDetailView.as_view(
