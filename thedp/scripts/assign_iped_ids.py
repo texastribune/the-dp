d68a14ae8fe2b0d357aa73cf0357893d03f8e15a
crccheck <c@crccheck.com>
2012-06-13 19:18:12 -0500

    add script to try to get ipeds id from a special file
    file not included.
    requires pulling a list of institutions from the IPEDS Data Center
    http://nces.ed.gov/ipeds/datacenter/Snapshotx.aspx (that url may or may
    not work)

---
+++ thedp/scripts/assign_iped_ids.py
@@ -0,0 +1,51 @@
+import sys
+
+from htmltableDictReader import DictReader
+from nltk.metrics import edit_distance as distance
+
+from thedp.models import Institution
+
+filename = sys.argv[1]
+
+reader = DictReader(open(filename, 'r'))
+
+qs = Institution.objects.filter(ipeds_id__isnull=True)
+total = qs.count()
+count = 0
+
+# pass one, exact matches
+maybes = []
+for row in reader:
+    try:
+        inst = qs.get(name=row['Name'])
+        inst.ipeds_id = row['UnitID']
+        inst.save()
+        print inst
+        count += 1
+        continue
+    except Institution.DoesNotExist:
+        pass
+        # print "MISSING:", row['Name']
+    maybes.append(row)
+    # print row
+
+print "Expected %s Found %s" % (total, count)
+
+
+def closest(name):
+    for m in maybes:
+        m['distance'] = distance(Institution.get_unique_name(name), Institution.get_unique_name(m['Name']))
+    ordered = sorted(maybes, key=lambda x: x['distance'])
+    return ordered[0], ordered[1]['distance'] - ordered[0]['distance']
+
+# pass two, fun stuff
+qs = Institution.objects.filter(ipeds_id__isnull=True)
+haves = Institution.objects.filter(ipeds_id__isnull=False).values_list('ipeds_id', flat=True)
+maybes = [x for x in maybes if int(x['UnitID']) not in haves]
+for inst in qs:
+    guess, confidence = closest(inst.name)
+    if confidence > 0:
+        if guess['distance'] == 0:
+            inst.ipeds_id = guess['UnitID']
+            inst.save()
+    print "%d +%d\t" % (guess['distance'], confidence), inst.name, "==", guess['Name']
