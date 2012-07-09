937ec866ce49a79915fb8335651ab127cad0ac20
crccheck <c@crccheck.com>
2012-07-09 15:51:37 -0500

    enable it in the admin

--- ipeds_importer/models.py
+++ ipeds_importer/models.py
@@ -8,3 +8,13 @@ class Variable(models.Model):
     category = models.CharField(max_length=150)
     long_name = models.CharField(max_length=80)
     raw = models.CharField(max_length=800, unique=True)
+
+
+from django.contrib import admin
+
+
+class VariableAdmin(admin.ModelAdmin):
+    list_display = ('code', 'short_name', 'category', 'long_name')
+    list_filter = ('code', 'short_name')
+
+admin.site.register(Variable, VariableAdmin)
