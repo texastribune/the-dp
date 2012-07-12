230dc90d5e439906555ab1a9dc8400fe03b4f378
crccheck <c@crccheck.com>
2012-07-12 13:54:04 -0500

    rename to import_customreport

--- bin/load.sh
+++ bin/load.sh
@@ -4,8 +4,8 @@ cd ../tx_highered/scripts/
 # These locations are specific to my particular setup
 
 # new style
-python import_prices.py prices /Users/crc/Dropbox/Data/Education-Higher/IPEDS/custom_reports/price.csv
-python import_prices.py testscores /Users/crc/Dropbox/Data/Education-Higher/IPEDS/custom_reports/testscores.csv
+python import_customreport.py prices /Users/crc/Dropbox/Data/Education-Higher/IPEDS/custom_reports/price.csv
+python import_customreport.py testscores /Users/crc/Dropbox/Data/Education-Higher/IPEDS/custom_reports/testscores.csv
 
 # old style auto-discover
 python import_report.py /Users/crc/Dropbox/Data/Education-Higher/IPEDS/reports
