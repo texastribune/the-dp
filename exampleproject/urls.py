fb7a8908c437b755da3744cb5e2e6588cc3c82ee
crccheck <c@crccheck.com>
2012-07-16 19:30:43 -0500

    steal 500 page from tt

--- exampleproject/urls.py
+++ exampleproject/urls.py
@@ -3,6 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+
+from django.views.generic import View
+class Http500View(View):
+    def get(self, *args, **kwargs):
+        raise Exception("One expects to simply walk into Mordor?")
+
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tx_highered.views.home', name='home'),
@@ -10,6 +17,7 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^500/$', Http500View.as_view()),
 
     url(r'^admin/', include(admin.site.urls)),
 )
