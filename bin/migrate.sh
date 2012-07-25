1377110eff2f451963212d093fb6e8f626246dd5
crccheck <c@crccheck.com>
2012-07-25 14:34:01 -0500

    remove postgis template since postgis is stripped out for creating the postgres table

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -2,7 +2,8 @@ cd $(cat $VIRTUAL_ENV/.project)/exampleproject
 
 # rm db.sqlite
 dropdb tx_highered
-createdb -T template_postgis tx_highered
+# createdb -T template_postgis tx_highered
+createdb tx_highered
 
 python manage.py syncdb --noinput
 
