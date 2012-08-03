3d0a83c2a8959db2ecc89d2ded58b2ef5d3f35b6
crccheck <c@crccheck.com>
2012-08-03 02:48:36 +0000

    disallow robots in robots.txt
    adapted from
    http://fredericiana.com/2010/06/09/three-ways-to-add-a-robots-txt-to-your-django-project/

--- exampleproject/urls.py
+++ exampleproject/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from django.http import HttpResponse
+
 admin.autodiscover()
 
-
 from django.views.generic import View
+
+
 class Http500View(View):
     def get(self, *args, **kwargs):
         raise Exception("One expects to simply walk into Mordor?")
@@ -17,7 +19,8 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^500/$', Http500View.as_view()),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /",
+        mimetype="text/plain")),
+    url(r'^500/$', Http500View.as_view()),
 )
