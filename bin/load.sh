a53b3bf17ba209ce638fa3ca5e1e010812ab093c
crccheck <c@crccheck.com>
2012-08-06 18:44:36 -0500

    add top 10 rule to default set of data to load

--- bin/load.sh
+++ bin/load.sh
@@ -4,10 +4,13 @@ cd $(cat $VIRTUAL_ENV/.project)/tx_highered/scripts
 DJANGO_SETTINGS_MODULE=exampleproject.settings
 
 # new style reports
+# ipeds
 python import_customreport.py $HOME/Dropbox/Data/Education-Higher/IPEDS/custom_reports/prices.csv
 python import_customreport.py $HOME/Dropbox/Data/Education-Higher/IPEDS/custom_reports/testscores.csv
 python import_customreport.py $HOME/Dropbox/Data/Education-Higher/IPEDS/custom_reports/enrollment.csv
 python import_customreport.py $HOME/Dropbox/Data/Education-Higher/IPEDS/custom_reports/grad_rates.csv
+# thecb
+./import_thecb_report.py $HOME/Dropbox/Data/Education-Higher/THECB/custom_reports/top_10_percent.html
 
 # old style auto-discover
 python import_report.py $HOME/Dropbox/Data/Education-Higher/IPEDS/reports
