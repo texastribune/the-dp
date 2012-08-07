99c0eea5cb4f00a018c35693b60c3d95bfc58d5d
crccheck <c@crccheck.com>
2012-08-07 19:14:56 +0000

    tweak admin, list institutions in alpha order

--- tx_highered/admin.py
+++ tx_highered/admin.py
@@ -6,6 +6,7 @@ class InstitutionAdmin(admin.ModelAdmin):
     list_display = ('name', 'ipeds_id', 'fice_id', 'ope_id')
     list_filter = ('institution_type', 'is_private')
     list_editable = ('ipeds_id', 'fice_id', 'ope_id')
+    ordering = ('name', )
 
 admin.site.register(Institution, InstitutionAdmin)
 admin.site.register(System, admin.ModelAdmin)
