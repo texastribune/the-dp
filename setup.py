dfb5a1146b44979f98dc8aa236f52f0393eeabab
Travis Swicegood <development@domain51.com>
2012-08-14 16:14:17 -0500

    Add a basic setup.py

---
+++ setup.py
@@ -0,0 +1,52 @@
+from distutils.core import setup
+import os.path
+
+
+def is_package(path):
+    return (
+        os.path.isdir(path) and
+        os.path.isfile(os.path.join(path, '__init__.py'))
+        )
+
+
+def find_packages(path, base=""):
+    """ Find all packages in path """
+    packages = {}
+    for item in os.listdir(path):
+        dir = os.path.join(path, item)
+        if is_package(dir):
+            if base:
+                module_name = "%(base)s.%(item)s" % vars()
+            else:
+                module_name = item
+            packages[module_name] = dir
+            packages.update(find_packages(dir, module_name))
+    return packages
+
+setup(
+    name='tx_highered',
+    version='0.1.0alpha',
+    description='Django app for Texas higher education data',
+    author='Texas Tribune',
+    author_email='tech@texastribune.org',
+    url='http://github.com/texastribune/tx_highered/',
+    license='Apache Software License',
+    install_requires=[
+    ],
+    packages=find_packages('./tx_highered', 'tx_highered').keys(),
+    package_data={
+        'tx_highered': [
+            'tx_highered/fixtures/*.json',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Other/NonlistedTopic'
+    ],
+)
