458aefd1b2e5a5975014c8d8cf0d6387aa230396
Travis Swicegood <development@domain51.com>
2012-08-16 17:46:32 -0500

    Rename variables for clarity

--- exampleproject/tx_highered_test/tests/models/reports.py
+++ exampleproject/tx_highered_test/tests/models/reports.py
@@ -5,16 +5,24 @@ from tx_highered import models
 class PriceTrendsTestCase(TestCase):
     def setUp(self):
         self.ut = models.Institution.objects.get(name__endswith='at Austin')
-        self.latest = self.ut.latest_tuition
+        self.price_trends = self.ut.latest_tuition
+
+    def test_in_state_maps_to_field(self):
+        self.assertEqual(self.price_trends.in_state,
+                self.price_trends.tuition_fees_in_state)
+
+    def test_out_of_state_maps_to_field(self):
+        self.assertEqual(self.price_trends.out_of_state,
+                self.price_trends.tuition_fees_outof_state)
 
     def test_a_decade_ago_returns(self):
-        a_decade_ago = self.latest.a_decade_ago
-        self.assertTrue(a_decade_ago.year, self.latest.year - 10)
+        a_decade_ago = self.price_trends.a_decade_ago
+        self.assertTrue(a_decade_ago.year, self.price_trends.year - 10)
 
     def test_in_state_has_risen_true(self):
-        self.assertTrue(self.latest.in_state_has_risen)
+        self.assertTrue(self.price_trends.in_state_has_risen)
 
     def test_in_state_has_risen_false(self):
-        old = self.latest.a_decade_ago
-        old.tuition_fees_in_state = self.latest.in_state + 1
-        self.assertFalse(self.latest.in_state_has_risen)
+        old = self.price_trends.a_decade_ago
+        old.tuition_fees_in_state = self.price_trends.in_state + 1
+        self.assertFalse(self.price_trends.in_state_has_risen)
