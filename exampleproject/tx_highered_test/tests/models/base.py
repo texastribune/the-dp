dd76c6dbb6078c5f8942abccd40572e2fe2cd16a
Travis Swicegood <development@domain51.com>
2012-08-17 12:01:43 -0500

    Add in missing tests from code that was created yesterday

---
+++ exampleproject/tx_highered_test/tests/models/base.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from tx_highered import models
+
+
+class InstitutionTestCase(TestCase):
+    def setUp(self):
+        self.obj = models.Institution.objects.get(name__endswith='at Austin')
+
+    def test_latest_tuition_returns_latest(self):
+        latest = self.obj.pricetrends_set.latest('year')
+        self.assertEqual(latest, self.obj.latest_tuition)
+
+    def test_number_of_full_time_students(self):
+        full_time = self.obj.enrollment_set.latest('year').fulltime
+        self.assertEqual(full_time, self.obj.number_of_full_time_students)
+
+    def test_sentences_property_returns_SummarySentences_object(self):
+        self.assertTrue(isinstance(self.obj.sentences,
+                models.SummarySentences))
