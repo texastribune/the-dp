05457d4eeee7d5b440a7eb2753df57e721b32727
crccheck <c@crccheck.com>
2012-08-22 01:14:14 +0000

    switch back to postgis database

--- exampleproject/settings/base.py
+++ exampleproject/settings/base.py
@@ -18,18 +18,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.contrib.gis.db.backends.postgis',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tx_highered',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-DATABASES = {'default': dj_database_url.config(default='postgres:///tx_highered')}
+DATABASES = {'default': dj_database_url.config(default='postgis:///tx_highered')}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
