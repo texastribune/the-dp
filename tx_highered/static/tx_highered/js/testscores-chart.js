68d1945197391c08e1e2133bf64606d1b7668d2d
crccheck <c@crccheck.com>
2012-08-23 15:37:03 -0500

    process the data so it's ready to be plotted

--- tx_highered/static/tx_highered/js/testscores-chart.js
+++ tx_highered/static/tx_highered/js/testscores-chart.js
@@ -3,6 +3,7 @@
 
 var TestScoresChart = function(){
   var url;
+  var _data;
   var data;
 
   // PUBLIC
@@ -14,7 +15,41 @@ var TestScoresChart = function(){
   function get_data_from_url(url){
     d3.json(url, function(new_data) {
       data = new_data;
+      _data = data;  // save reference to original raw data;
+      process_data(data);
     });
+  }
+
+  function process_data(new_data){
+    // only do SAT
+    /*
+      from:
+      [{year: 2011, sat:{verbal: [530, 730], act:{}}}]
+
+      to:
+      [{x: 2011, y_min: 530, y_max: 730, type: 'verbal'}]
+    */
+    var processed_data = {verbal:[], math:[], writing:[]};
+    for (var i = 0; i < new_data.length; i++){
+      var datum = new_data[i];
+      processed_data.verbal.push({
+        x: datum.year,
+        y_min: datum.sat.verbal[0],
+        y_max: datum.sat.verbal[1]
+      });
+      processed_data.math.push({
+        x: datum.year,
+        y_min: datum.sat.math[0],
+        y_max: datum.sat.math[1]
+      });
+      processed_data.writing.push({
+        x: datum.year,
+        y_min: datum.sat.writing[0],
+        y_max: datum.sat.writing[1]
+      });
+    }
+    data = processed_data;
+    return data;
   }
 
   // PUBLIC
