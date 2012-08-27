cc8939bec851f50880050a07f9b713b6181c1116
crccheck <c@crccheck.com>
2012-08-27 18:04:30 -0500

    copy D3GroupedBarChart logic over to make a custom class

--- tx_highered/static/tx_highered/js/gradrates-chart.js
+++ tx_highered/static/tx_highered/js/gradrates-chart.js
@@ -1,8 +1,35 @@
+var GradRatesChart = D3BarChart.extend();
+
+GradRatesChart.prototype.get_layers = function(){
+  // set up a layer for each series
+  var self = this;
+  var layers = self.plot.selectAll("g.layer")
+    .data(this._data)
+    .enter().append("g")
+      .attr("class", "layer")
+      .style("fill", function(d, i) { return self.options.color(i); });
+  // shift grouped bars so they're adjacent to each other
+  layers
+    .attr("transform", function(d, i) {
+      var offset = self.bar_width * 0.9 * i;
+      return "translate(" + offset + ",0)";
+    });
+  return layers;
+};
+
+GradRatesChart.prototype.get_bar_width = function(){
+  var len_series = this._data.length; // m, i, rows
+  var len_x = this._data[0].length;   // n, j, cols
+  var bar_width = this.options.plot_box.w / len_x;  // bar_width is an outer width
+  bar_width = bar_width / len_series;
+  return bar_width;
+};
+
   var $section = $('#gradrates');
   var $source = $section.find('table');
   var data = $source.tabulate();
 
-  var chart = new D3GroupedBarChart($('<div class="chart" />').appendTo($section)[0],
+  var chart = new GradRatesChart($('<div class="chart" />').appendTo($section)[0],
         data,
         {
           'color': d3.scale.ordinal().range(['#99c', '#639', '#306'])
