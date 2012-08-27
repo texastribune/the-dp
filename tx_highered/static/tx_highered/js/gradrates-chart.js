ad5e65ce856510a7c056276b714b3023edb9eb72
crccheck <c@crccheck.com>
2012-08-27 17:36:55 -0500

    rough in chart as a grouped bar chart

--- tx_highered/static/tx_highered/js/gradrates-chart.js
+++ tx_highered/static/tx_highered/js/gradrates-chart.js
@@ -1,3 +1,14 @@
   var $section = $('#gradrates');
   var $source = $section.find('table');
   var data = $source.tabulate();
+
+  var chart = new D3GroupedBarChart($('<div class="chart" />').appendTo($section)[0],
+        data,
+        {
+          'color': d3.scale.ordinal().range(['#99c', '#639', '#306'])
+        });
+
+  chart.get_y_domain = function(){
+    return [0, 100];
+  };
+  chart.refresh();
