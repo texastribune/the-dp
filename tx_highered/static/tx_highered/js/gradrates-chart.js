c64a09442e022b9bda032735cd32e0218d62f702
crccheck <c@crccheck.com>
2012-08-28 11:24:16 -0500

    update visual polish of grad rates chart

--- tx_highered/static/tx_highered/js/gradrates-chart.js
+++ tx_highered/static/tx_highered/js/gradrates-chart.js
@@ -1,45 +1,54 @@
-var GradRatesChart = D3BarChart.extend();
+(function(){
+  var GradRatesChart = D3BarChart.extend();
 
-GradRatesChart.prototype.get_layers = function(){
-  // set up a layer for each series
-  var self = this;
-  var layers = self.plot.selectAll("g.layer")
-    .data(this._data)
-    .enter().append("g")
-      .attr("class", "layer")
-      .style("fill", function(d, i) { return self.options.color(i); });
-  // shift grouped bars so they're adjacent to each other
-  layers
-    .attr("transform", function(d, i) {
-      var offset = 20 - 10 * i;
-      return "translate(" + offset + ",0)";
-    });
-  return layers;
-};
+  GradRatesChart.prototype.get_layers = function(){
+    // set up a layer for each series
+    var self = this;
+    var layers = self.plot.selectAll("g.layer")
+      .data(this._data)
+      .enter().append("g")
+        .attr("class", "layer")
+        .style("fill", function(d, i) { return self.options.color(i); });
+    // shift grouped bars so they're adjacent to each other
+    layers
+      .attr("transform", function(d, i) {
+        var offset = 20 - 10 * i;
+        return "translate(" + offset + ",0)";
+      });
+    return layers;
+  };
 
-GradRatesChart.prototype.get_bar_width = function(){
-  var len_series = this._data.length; // m, i, rows
-  var len_x = this._data[0].length;   // n, j, cols
-  var bar_width = this.options.plot_box.w / len_x;  // bar_width is an outer width
-  bar_width = bar_width - 20 - 10;
-  return bar_width;
-};
+  GradRatesChart.prototype.get_bar_width = function(){
+    var len_series = this._data.length; // m, i, rows
+    var len_x = this._data[0].length;   // n, j, cols
+    var bar_width = this.options.plot_box.w / len_x;  // bar_width is an outer width
+    bar_width = bar_width - 20 - 10;
+    return bar_width;
+  };
 
+  // y-axis shows percentage
+  GradRatesChart.prototype.get_y_domain = function(){
+    return [0, 100];
+  };
 
+  // BEGIN
   var $section = $('#gradrates');
   var $source = $section.find('table');
   var data = $source.tabulate();
-  data = [data[2], data[1], data[0]];  // reverse
   var colors = ['#99c', '#639', '#306'];
-  colors = [colors[2], colors[1], colors[0]];
+  data = [data[2], data[1], data[0]];  // reverse
+  colors = [colors[2], colors[1], colors[0]]; //reverse
 
-  var chart = new GradRatesChart($('<div class="chart" />').appendTo($section)[0],
+  var chart = new GradRatesChart(
+        $('<div class="chart" />').appendTo($section)[0],
         data,
         {
-          'color': d3.scale.ordinal().range(colors)
+          'color': d3.scale.ordinal().range(colors),
+          'tooltip': function(){
+            var d = this.__data__;
+            return d.series + " bachelor's graduation rate<br><b>" +
+              d3.format(",.2f")(this.__data__.y) + "%</b>";
+          }
         });
 
-  chart.get_y_domain = function(){
-    return [0, 100];
-  };
-  chart.refresh();
+})();
