c4228ddd47249a5c7fb0f1475e87faf23e885eb7
crccheck <c@crccheck.com>
2012-08-29 18:43:38 -0500

    remove institutions path for schools' urls
    as per @tswicegood's suggestion

--- tx_highered/urls.py
+++ tx_highered/urls.py
@@ -11,9 +11,8 @@ from . import api
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name="home"),
-    url(r'^institutions/$', InstitutionListView.as_view(), name="institution_list"),
-    url(r'^institutions/(?P<slug>[-\w]+)/$', InstitutionDetailView.as_view(),
-        name="institution_detail"),
+    url(r'^institutions/$', InstitutionListView.as_view(),
+        name="institution_list"),
     url(r'^system/$', ListView.as_view(
         model=System), name="system_list"),
     url(r'^system/(?P<slug>[-\w]+)/$', RenderModelDetailView.as_view(
@@ -22,6 +21,8 @@ urlpatterns = patterns('',
     url(r'^testing/$', TestScoresReport.as_view(), name="sat_search"),
     url(r'^funnel/$', FunnelReport.as_view()),
     url(r'^top10/$', Top10RuleReport.as_view()),
+    url(r'^(?P<slug>[-\w]+)/$', InstitutionDetailView.as_view(),
+        name="institution_detail"),
 ) + patterns('tx_highered.api',
     url(r'^api/institution/(?P<pk>\d+)/enrollment/$', 'enrollment_api',
         name='enrollment_api'),
