443e479799184618c5d3f8201e6113651f1af07e
Travis Swicegood <development@domain51.com>
2012-08-31 11:11:18 -0500

    JS styling

--- tx_highered/static/tx_highered/js/tabbouleh.js
+++ tx_highered/static/tx_highered/js/tabbouleh.js
@@ -1,43 +1,41 @@
-;(function($, window) {
-  function getCellValue(cell) {
-    return (+ $(cell).data("value"));
-  }
+(function($) {
+  var getCellValue = function(cell) {
+        return (+ $(cell).data("value"));
+      },
 
-  function getCellLabel(header, x, y) {
-    return header + " " + x + " <strong>" + y + "</strong>";
-  }
+      getCellLabel = function(header, x, y) {
+        return header + " " + x + " <strong>" + y + "</strong>";
+      },
 
-  function tabulate(table) {
-    var $table = $(table);
+      tabulate = function(table) {
+        var $table = $(table),
+            // Use the first row as column headers
+            headers = $table.find('thead th').map(function() {
+              return $(this).text();
+            }),
+            // Initialize a list of data for each column except the first
+            data = headers.slice(1).toArray().map(function() {
+              return [];
+            });
 
-    // Use the first row as column headers
-    var headers = $table.find('thead th').map(function() {
-      return $(this).text();
-    });
+        $table.find('tbody > tr').each(function(_, row) {
+          // Take the first cell as the x value
+          var x = getCellValue($(row).children().eq(0));
 
-    // Initialize a list of data for each column except the first
-    var data = headers.slice(1).toArray().map(function() {
-      return [];
-    });
-
-    $table.find('tbody > tr').each(function(_, row) {
-      // Take the first cell as the x value
-      var x = getCellValue($(row).children().eq(0));
-
-      // Append the remaining cells to their column data
-      $(row).children().slice(1).each(function(index, cell) {
-        var y = getCellValue(cell);
-        data[index].push({
-          x: x,
-          y: y,
-          series: headers[index + 1],
-          title: getCellLabel(headers[index + 1], x, y)
+          // Append the remaining cells to their column data
+          $(row).children().slice(1).each(function(index, cell) {
+            var y = getCellValue(cell);
+            data[index].push({
+              x: x,
+              y: y,
+              series: headers[index + 1],
+              title: getCellLabel(headers[index + 1], x, y)
+            });
+          });
         });
-      });
-    });
 
-    return data;
-  }
+        return data;
+      };
 
   /**
    * The jQuery.tabulate plugin.
@@ -53,4 +51,4 @@
       return tabulate(table);
     });
   };
-})(jQuery, window);
+})(jQuery);
