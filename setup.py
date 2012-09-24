d07da4b8ebb0d09da49c92ac81b290b821c3d7c9
crccheck <c@crccheck.com>
2012-09-24 16:18:24 -0500

    use setuptools's find_packages instead of a custom one
    fixes missing .py files in the base module, setup.py could not get
    version

--- setup.py
+++ setup.py
@@ -1,29 +1,7 @@
+from setuptools import find_packages
 from distutils.core import setup
-import os.path
 
 import tx_highered
-
-
-def is_package(path):
-    return (
-        os.path.isdir(path) and
-        os.path.isfile(os.path.join(path, '__init__.py'))
-        )
-
-
-def find_packages(path, base=""):
-    """ Find all packages in path """
-    packages = {}
-    for item in os.listdir(path):
-        dir = os.path.join(path, item)
-        if is_package(dir):
-            if base:
-                module_name = "%(base)s.%(item)s" % vars()
-            else:
-                module_name = item
-            packages[module_name] = dir
-            packages.update(find_packages(dir, module_name))
-    return packages
 
 setup(
     name='tx_highered',
@@ -36,7 +14,7 @@ setup(
     install_requires=[
         'geopy',
     ],
-    packages=find_packages('./tx_highered', 'tx_highered').keys(),
+    packages=find_packages('.', exclude=('exampleproject*',)),
     package_data={
         'tx_highered': [
             'tx_highered/fixtures/*.json',
