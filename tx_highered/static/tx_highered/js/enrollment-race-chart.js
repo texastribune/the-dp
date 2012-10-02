294e8a1d139b561429880757e6d2c181b02f1dbc
crccheck <c@crccheck.com>
2012-10-02 16:32:02 +0000

    get race/ethnicity chart rendered

--- tx_highered/static/tx_highered/js/enrollment-race-chart.js
+++ tx_highered/static/tx_highered/js/enrollment-race-chart.js
@@ -1,4 +1,4 @@
-/*globals $, d3, D3StackedBarChart, enrollment_chart_url */
+/*globals $, d3, D3StackedBarChart, enrollment_chart_url, __extends */
 (function() {
   "use strict";
 
@@ -20,112 +20,57 @@
         "total_percent_unknown": "#CC0024"
       };
 
-  // private functions from
-  // https://raw.github.com/mbostock/d3/master/src/layout/stack.js
-  // for re-ordering data
-  function d3_layout_stackReduceSum(d) {
-    return d.reduce(d3_layout_stackSum, 0);
-  }
+  var CustomChart = (function() {
 
-  function d3_layout_stackSum(p, d) {
-    return p + d[1];
-  }
+    __extends(C, D3StackedBarChart);
 
-  // similar to python's any
-  function any(arr){
-    return arr.reduce(function(a, b){ return a || b; });
-  }
+    function C() {
+      return C.__super__.constructor.apply(this, arguments);
+    }
 
-  var Chart = D3StackedBarChart.extend({
-    initData: function(new_data){
+    C.prototype.initData = function(new_data){
       // override to process data differently
       this.originalData = new_data;  // hold onto this
       var nested_dataset = d3.nest()
           .key(function(d) { return d.metric; })
           .entries(new_data);
-      var stackOrder,
-          stack = d3.layout.stack()
-          .offset("zero")  // default
-          .order(function(data) {
-            var n = data.length,
-                sums = data.map(d3_layout_stackReduceSum);
-            stackOrder = d3.range(n).sort(function(a, b) { return sums[b] - sums[a]; });
-            return stackOrder;
-          })
-          .values(function(d) { return d.values; })
-          .x(function(d) { return d.year; })  // this doesn't seem to work
-          .y(function(d) { return d.value; });
-      var stacked_dataset = stack(nested_dataset);
-      // actually sort the data too so colors are applied in sort order
-      var return_data = [];
-      for (var i = 0; i < stackOrder.length; i++){
-        return_data.push(stacked_dataset[stackOrder[i]]);
-      }
-      // slower but terser alternative
-      // return stackOrder.map(function(x){ return stacked_dataset[x]; });
-      return return_data;
-    },
-    getXScale: function(){
+      return C.__super__.initData.call(this, nested_dataset);
+    };
+
+    C.prototype.getXScale = function(){
       // override to pull year range from original data
       var self = this,
           data = this.originalData;
       var yearRange = d3.extent(data, function(d) { return d.year; });
       return d3.scale.ordinal()
           .domain(d3.range(yearRange[0], yearRange[1] + 1))
-          .rangeRoundBands([0, self.options.plot_box.w], 0.1, 0.1);
-    },
-    getMaxY: function(data){
+          .rangeRoundBands([0, self.options.plotBox.width], 0.1, 0.1);
+    };
+
+    C.prototype.getMaxY = function(data){
       // pull max from original raw data
       return d3.max(this.originalData, function(d) { return d.enrollment; });
-    },
-    getLayerFillStyle: function(){
-      var self = this,
-          n = self._data.length ;
-      return function(d, i) {
-        return self.options.color[d.key]; };
-    },
-    getX: function(){
+    };
+
+    C.prototype.getX = function(){
       // override to use `d.year` instead of `d.x`
       var self = this;
-      return function(d) { return self.x_scale(d.year); };
-    },
-    getY: function(){
+      return function(d) { return self.xScale(d.year); };
+    };
+
+    C.prototype.getY = function(){
       // override to convert percent to value
       var self = this;
-      return function(d) { return self.y_scale((d.y0 + d.y) * d.enrollment / 100.0); };
-    },
-    getH: function(){
+      return function(d) { return self.yScale((d.y0 + d.y) * d.enrollment / 100.0); };
+    };
+
+    C.prototype.getH = function(){
       // override to convert percent to value
       var self = this;
-      return function(d) { return self.height_scale(d.y * d.enrollment / 100.0); };
-    },
-    getBarWidth: function(){
-      // override to get `len_x` a different way TODO can probably port this back into super
-      // bar_width is an outer width
-      return this.options.plot_box.w / this.x_scale.domain().length;
-    },
-    getBars: function(){
-      // override to fix different data structure, array of objects instead of array of arrays
-      var self = this;
-      return this._layers.selectAll("rect.bar")
-        .data(function(d) { return d.values; })
-        .enter().append("rect")
-          .attr("class", "bar")
-          .attr("width", self.bar_width * 0.9)
-          .attr("x", self.x)
-          .attr("y", self.options.plot_box.h)
-          .attr("height", 0)
-          .transition()
-            .delay(function(d, i) { return i * 10; })
-            .attr("y", self.y)
-            .attr("height", self.h);
-    },
+      return function(d) { return self.hScale(d.y * d.enrollment / 100.0); };
+    };
 
-    postRenderLegend: function(el){
-      $(el).find("li.inactive").removeClass('inactive');
-    },
-
-    legendActivateSeries: function(idx, targetElem){
+    C.prototype.legendActivateSeries = function(idx, targetElem){
       var self = this, filtered, targeted,
           $target = $(targetElem).parent(),
           $set = $target.parent().children(':not(.reset)'),
@@ -196,12 +141,14 @@
       if (self.yAxis){
         self.svg.select('.y.axis').transition().call(self.yAxis);
       }
-    },
+    };
 
-    getLegendSeriesTitle: function(d, i){
-      return d.values[0].race.substr(2);
-    }
-  });
+    C.prototype.postRender = function(el){
+      $(el).find("li.inactive").removeClass('inactive');
+    };
+
+    return C;
+  })();
 
   var tooltipFmt = function(d){
         var guess = d3.format(",d")(Math.round(d.y * d.enrollment / 100));
@@ -210,8 +157,16 @@
       options = {
         // 'color': d3.interpolateRgb("#001", "#eef"),  // does not actually reach maxima
         // 'color': d3.scale.pow().exponent(0.75).range(["#445", "#ccd"]),
-        color: COLORS,
-        tooltip: function() { return tooltipFmt(this.__data__); },
+        colors: COLORS,
+        accessors: {
+          bars: function(d) { return d.values; },
+          colors: function(d, i) { return d.key; },
+          y: function(d) { return d.value; }
+        },
+        tooltip: {
+          enabled: true,
+          format: function() { return tooltipFmt(this.__data__); }
+        },
         xAxis: {
           enabled: true,
           title: "Year"
@@ -222,9 +177,11 @@
         },
         legend: {
           enabled: true,
-          elem: $("#enrollment .legend")
-        }
+          elem: $("#enrollment .legend"),
+          titleAccessor: function(d){ return d.values[0].race.substr(2); }
+        },
+        stackOrder: "big-bottom"
       };
 
-  new Chart($("#enrollment .chart"), enrollment_chart_url, options);
+  window.z = new CustomChart($("#enrollment .chart"), enrollment_chart_url, options);
 })();
