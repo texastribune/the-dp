3706c9fd8b735470d7c9c3f6974f04d2ca517f74
Noah Seger <noah@texastribune.org>
2013-07-02 13:55:27 -0500

    Remove extinct webdesign app

--- exampleproject/settings/base.py
+++ exampleproject/settings/base.py
@@ -126,7 +126,6 @@ INSTALLED_APPS = [
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'django.contrib.humanize',
-    'django.contrib.webdesign',
     'django.contrib.gis',
 
     'tx_highered',
--- exampleproject/settings/dev.py
+++ exampleproject/settings/dev.py
@@ -3,5 +3,4 @@ from .base import *
 INSTALLED_APPS.extend([
     # 'south',
     'django_extensions',
-    # 'django.contrib.webdesign',
 ])
