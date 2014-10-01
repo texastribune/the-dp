261e52effc26bd49951098ccce10fb8db7474c72
crccheck <c@crccheck.com>
2014-10-01 14:17:39 -0500

    deprecate graduation rate data from thecb almanac

--- bin/load.sh
+++ bin/load.sh
@@ -19,8 +19,4 @@ done
 # TODO make it so the directory structure doesn't matter for ipeds/thecb csvs
 # python tx_highered/scripts/import_thecb_report.py $HIGHER_ED_DATA/THECB/custom_reports/top_10_percent.html
 # python tx_highered/thecb_importer/load_enrollment.py
-# python tx_highered/thecb_importer/load_graduation_rates.py $HIGHER_ED_DATA/THECB/custom_reports/university_graduation_rates.xls.html
-# python tx_highered/thecb_importer/load_graduation_rates.py $HIGHER_ED_DATA/THECB/custom_reports/community_graduation_rates.xls.html
-# python tx_highered/thecb_importer/load_graduation_rates.py $HIGHER_ED_DATA/THECB/custom_reports/state_graduation_rates.xls.html
-# python tx_highered/thecb_importer/load_almanac_graduation_rates.py $HIGHER_ED_DATA/THECB/Almanac/almanac_p21.pdf.json
 # python tx_highered/thecb_importer/load_admissions.py $HIGHER_ED_DATA/THECB/Admissions/
