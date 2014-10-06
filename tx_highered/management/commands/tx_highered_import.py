e9ec3a7ea22540c10b6db3e78fe2c5299e127119
crccheck <c@crccheck.com>
2014-10-06 18:04:01 -0500

    update importer management command to prep for multiple importers

--- tx_highered/management/commands/tx_highered_import.py
+++ tx_highered/management/commands/tx_highered_import.py
@@ -2,14 +2,17 @@ import os
 
 from django.core.management.base import BaseCommand
 
+from tx_highered.scripts.import_customreport import generic
+
 
 class Command(BaseCommand):
+    args = '(ipeds|thecb) <file file ...>'
     help = "Import Data"
 
-    def handle(self, *args, **options):
+    def handle(self, importer_type, *args, **options):
         # TODO handle THECB data
-        from tx_highered.scripts.import_customreport import generic
 
-        for path in args:
-            if os.path.isfile(path):
-                generic(path)
+        if importer_type == 'ipeds':
+            for path in args:
+                if os.path.isfile(path):
+                    generic(path)
