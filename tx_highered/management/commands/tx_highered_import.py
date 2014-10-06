a3091b3ad0ce95dffedd8b2d9a8f9e72e36ad157
crccheck <c@crccheck.com>
2014-10-06 18:07:43 -0500

    add ability for importer to handle thecb data too

--- tx_highered/management/commands/tx_highered_import.py
+++ tx_highered/management/commands/tx_highered_import.py
@@ -1,8 +1,9 @@
 import os
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
-from tx_highered.scripts.import_customreport import generic
+from tx_highered.scripts.import_customreport import generic as ipeds
+from tx_highered.scripts.import_thecb_report import generic as thecb
 
 
 class Command(BaseCommand):
@@ -10,9 +11,16 @@ class Command(BaseCommand):
     help = "Import Data"
 
     def handle(self, importer_type, *args, **options):
-        # TODO handle THECB data
+        # WISHLIST handle verbosity option
 
         if importer_type == 'ipeds':
             for path in args:
                 if os.path.isfile(path):
-                    generic(path)
+                    ipeds(path)
+        elif importer_type == 'thecb':
+            for path in args:
+                if os.path.isfile(path):
+                    thecb(path)
+        else:
+            raise CommandError(u'Not a valid importer type: "{}"'
+                .format(importer_type))
